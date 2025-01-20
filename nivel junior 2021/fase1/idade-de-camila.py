db1b9674506f94f3994155e0ede6f7c3bb8272de
pedro <pedro.do.nascimento.braga@gmail.com>
2025-01-20 02:32:57 -0300

    criação plano-de-internet.py e aperfeiçoamento da idade-de-camila.py

--- nivel junior 2021/fase1/idade-de-camila.py
+++ nivel junior 2021/fase1/idade-de-camila.py
@@ -1,13 +1,18 @@
-idade1 = int(input())
-idade2 = int(input())
-idade3 = int(input())
+idades = input() , input() , input() #cria uma tuple
+idades = list(idades) #transforma a tuple em array
 
-if idade1 >= 5 and idade1 <= 100 and idade2 >= 5 and idade2 <= 100 and idade3 >= 5 and idade3 <= 100:
-    if idade1 < idade2 and idade1 > idade3 or idade1 > idade2 and idade1 < idade3: ##idade1
-        print(idade1)
 
-    elif idade2 < idade1 and idade2 > idade3 or idade2 > idade1 and idade2 < idade3: ##idade2
-        print(idade2)
+for cont in range (len(idades)):idades[cont] = int(idades[cont]) #loop para transformar em int, criado no ordenacaonumerica.py em exercícios
 
-    elif idade3 < idade2 and idade3 > idade1 or idade3 > idade2 and idade3 < idade1: ##idade3
-        print(idade3)
+if idades[0] >= 5 and idades[0] <= 100 and idades[1] >= 5 and idades[1] <= 100 and idades[2] >= 5 and idades[2] <= 100:
+
+    if idades[0] >= 5 and idades[0] <= 100 and idades[1] >= 5 and idades[1] <= 100 and idades[2] >= 5 and idades[2] <= 100:
+
+        if idades[0] < idades[1] and idades[0] > idades[2] or idades[0] > idades[1] and idades[0] < idades[2]: #idades[0]
+            print(idades[0])
+
+        elif idades[1] < idades[0] and idades[1] > idades[2] or idades[1] > idades[0] and idades[1] < idades[2]: #idades[1]
+            print(idades[1])
+
+        elif idades[2] < idades[1] and idades[2] > idades[0] or idades[2] > idades[1] and idades[2] < idades[0]: #idades[2]
+            print(idades[2])
